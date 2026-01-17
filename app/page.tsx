e8ac9eefe95032442577aea7ebe7215f9eac25e5
unknown <iprincepatel22@gmail.com>
2026-01-17 15:00:20 +0530

    Added base

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-6 lg:p-24">
+      <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+      <p className="mt-4 text-lg">We Will start From the beginning.</p>
+    </main>
+  );
+}
